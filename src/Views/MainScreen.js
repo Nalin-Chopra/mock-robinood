3f72a80da63256996ef70ddbc85a21f948202f60
Nalin <nalin.chopra@berkeley.edu>
2020-08-26 14:28:01 -0400

    working

--- src/Views/MainScreen.js
+++ src/Views/MainScreen.js
@@ -5,7 +5,6 @@ export function MainScreen(props) {
     return (
         <div> 
             <h1 style={{color: "#FFFF", fontSize: "15rem", marginBottom: "25rem", position: "relative"}}>MOCK ROBINHOOD</h1>
-
             <h2 style={{color: "#FFFF", fontSize: "5rem", marginBottom: "10rem", position: "relative"}}> SIMDAQ EXCHANGE</h2>
             <TickerList />
         </div>
--- src/Views/TickerList.js
+++ src/Views/TickerList.js
@@ -1,71 +1,50 @@
 import React, {useEffect, useState} from 'react';
 import {CompanyTile} from './CompanyTile/CompanyTile';
 
-const API_URL = 'https://mock-robinhood-server-e3jyc8q9t.vercel.app/stocks';
-
-let companies = [
-    {
-        name: "AAPL",
-        fullName: "Apple",
-        price: 458.83,
-        logo: "https://seeklogo.com/images/A/apple-logo-52C416BDDD-seeklogo.com.png",
-        description: "Apple Inc. designs, manufactures and markets mobile communication and media devices, personal computers and portable digital music players. The Company sells a range of related software, services, accessories, networking solutions, and third-party digital content and applications."
-    },
-    {
-        name: "FB",
-        fullName: "Facebook",
-        price: 261.16,
-        logo: "https://i0.wp.com/www.vectorico.com/wp-content/uploads/2018/02/Facebook-Logo.png?fit=600%2C600"
-    },
-    {
-        name: "GOOG",
-        fullName: "Google",
-        price: 1516.24,
-        logo: "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-png-webinar-optimizing-for-success-google-business-webinar-13.png"
-    },
-    {
-        name: "GTSBY",
-        fullName: "Gatsby",
-        price: 4990.12,
-        logo: "https://media-exp1.licdn.com/dms/image/C560BAQHuiTdDW58LOw/company-logo_200_200/0?e=1606348800&v=beta&t=DVVrNQXp9Kw6WAH0XqjvdXJUboA2N8yvkBkE0h0XA-s"
-    },
-    {
-        name: "AMZN",
-        fullName: "Amazon",
-        price: 3284.72,
-        logo: "https://pngimg.com/uploads/amazon/amazon_PNG5.png"
-    }
-
-]
+const API_URL = 'https://mock-robinhood-server-gvycz7jq1.vercel.app/stocks';
 
 export function TickerList(props) {
-    const [companyList, setCompanyList] = useState(companies);
+    const [companyList, setCompanyList] = useState([]);
 
     useEffect(() => {
         let interval;
         async function getStocks() {
-            let newStocks = []
-            fetch(API_URL)
-            .then(response => response.json())
-            .then(stocks => {
-                newStocks = stocks;
-            });
-            interval = setInterval(() => setCompanyList(newStocks), 1000);
+            try {
+                let newStocks = fetch(API_URL)
+                    .then(response => response.json())
+                    .then(stocks => {
+                        newStocks = stocks;
+                    });
+                if (newStocks.length === 0) {
+                    throw('Mock server returned empty stock data.');
+                }
+                interval = setInterval(() => setCompanyList(newStocks), 1500);
+            } catch {
+                interval = setInterval(() => setCompanyList([]), 1500);
+            }
         };
         getStocks();
         return () => {
             clearInterval(interval);
         }
-        
     });
-    let cardElems = companyList.map(company => <CompanyTile key={Date.now().toString() + Math.random() * 0.999} companyObj={company} />)
-
+    let cardElems = [];
+    if (companyList != undefined) {
+        cardElems = companyList.map(company => <CompanyTile key={company.name} companyObj={company} />)
+    }
+    
+    let noStocksToDisplay = companyList.length === 0;
+    const noStocks = (
+        <h1 style={{fontSize: "10rem", color: "#FFFFFF", position: "relative"}}>No stock data currently available.</h1>
+    );
     
     return (            
-
         <div className="container-fluid d-flex justify-content-center" >
             <div className="row">
                 {cardElems}
+            </div>
+            <div>
+                {noStocksToDisplay && noStocks} 
             </div>
         </div>
     )
